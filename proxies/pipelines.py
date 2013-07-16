b65a20ad39821b05414ac70fe78a738fc7c4d6ea
Blender3D <452469+Blender3D@users.noreply.github.com>
2013-07-16 01:31:55 +0000

    Fixed item format.

--- proxies/pipelines.py
+++ proxies/pipelines.py
@@ -1,5 +1,14 @@
+from scrapy.contrib.exporter import BaseItemExporter
 from scrapy.exceptions import DropItem
 
 class ProxiesPipeline(object):
     def process_item(self, item, spider):
         return item
+
+class FlatFileItemExporter(BaseItemExporter):
+    def __init__(self, file, **kwargs):
+        self._configure(kwargs)
+        self.file = file
+
+    def export_item(self, item):
+        self.file.write('{address}:{port}\n'.format(**item))
